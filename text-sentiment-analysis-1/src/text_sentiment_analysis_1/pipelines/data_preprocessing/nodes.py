3dda7e1bc120aadc1d0e7956f1087419c53a905c
Adam Maulana <gitarbetot@gmail.com>
2022-03-06 05:14:36 +0700

    feat: add text normalization func

--- text-sentiment-analysis-1/src/text_sentiment_analysis_1/pipelines/data_preprocessing/nodes.py
+++ text-sentiment-analysis-1/src/text_sentiment_analysis_1/pipelines/data_preprocessing/nodes.py
@@ -2,12 +2,14 @@
 This is a boilerplate pipeline 'data_preprocessing'
 generated using Kedro 0.17.7
 """
-import pandas as pd
-import xml.etree.ElementTree as ET
 from lxml import etree
 
+import pandas as pd
+import re
+import xml.etree.ElementTree as ET
+
 def _load_xml_tree_from_text(content: str) -> ET.ElementTree:
-    parser = etree.XMLParser(recover=True) # recover=True -> to skip broken format/characters
+    parser = etree.XMLParser(recover=True) # recover=T -> skip broken form
     return ET.ElementTree(ET.fromstring(content, parser=parser))
 
 def _convert_labelled_data_tree_to_dataframe(tree: ET.ElementTree) -> pd.DataFrame:
@@ -28,8 +30,10 @@ def _convert_labelled_data_tree_to_dataframe(tree: ET.ElementTree) -> pd.DataFra
         
         row_payload = {
             "rid": review_id, "text": review_text,
-            "food_0": "unknown", "price_0": "unknown", "ambience_0": "unknown", "service_0": "unknown",
-            "food_1": "unknown", "price_1": "unknown", "ambience_1": "unknown", "service_1": "unknown"
+            "food_0": "unknown", "price_0": "unknown", 
+            "ambience_0": "unknown", "service_0": "unknown",
+            "food_1": "unknown", "price_1": "unknown", 
+            "ambience_1": "unknown", "service_1": "unknown"
         }
 
         for reviewer in reviewers:
@@ -70,6 +74,19 @@ def _convert_labelled_data_tree_to_dataframe(tree: ET.ElementTree) -> pd.DataFra
     dataframe = pd.DataFrame(rows, columns = columns)
     return dataframe
 
+def _normalize_text(x: str) -> str:
+    x = x.strip()
+    x = x.lower()
+    x = re.sub(r's+', ' ', x) # replace trailing spaces into a single space
+    x = re.sub(r'[^\w\s]', '', x) # remove punctuations
+    x = re.sub(r'\d', '', x) # remove digits
+    x = re.sub(r'(.+?)\1+', r'\1\1', x) # replace repeating character (hahaha -> haha, heyyyy -> heyy)
+    x = re.sub('(\\b[A-Za-z] \\b|\\b [A-Za-z]\\b|\\b[A-Za-z]\\b)', '', x) # remove single letter
+    return x
+
+def _normalize_text_column(x: pd.Series) -> pd.Series:
+    x = x.apply(_normalize_text)
+    return x
 
 def preprocess_labelled_data(xml_content: str) -> pd.DataFrame:
     """Preprocess the data for labelled_data
