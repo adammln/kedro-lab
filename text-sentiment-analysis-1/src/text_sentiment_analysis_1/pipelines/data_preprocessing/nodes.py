949ff226927e07295ae425da784f6aef3df6f2bd
Adam Maulana <gitarbetot@gmail.com>
2022-03-06 09:12:24 +0700

    refactor: remove redundancy in XML converter

--- text-sentiment-analysis-1/src/text_sentiment_analysis_1/pipelines/data_preprocessing/nodes.py
+++ text-sentiment-analysis-1/src/text_sentiment_analysis_1/pipelines/data_preprocessing/nodes.py
@@ -14,20 +14,17 @@ def _load_xml_tree_from_text(content: str) -> ET.ElementTree:
 
 def _convert_labelled_data_tree_to_dataframe(tree: ET.ElementTree) -> pd.DataFrame:
     root = tree.getroot()
-    
     columns = [
         "rid", "text", "food_0", "price_0", "ambience_0", 
         "service_0", "food_1", "price_1", "ambience_1", 
         "service_1"
-    ]
-    
+    ] 
     rows = []
 
     for node in root:
         review_id = node.attrib.get("rid")
         review_text = node.find("text").text if node is not None else None
-        reviewers = node.findall("aspects")
-        
+        reviewers = node.findall("aspects")      
         row_payload = {
             "rid": review_id, "text": review_text,
             "food_0": "unknown", "price_0": "unknown", 
@@ -39,37 +36,16 @@ def _convert_labelled_data_tree_to_dataframe(tree: ET.ElementTree) -> pd.DataFra
         for reviewer in reviewers:
             temp_id = ''
             temp_polarity = ''
-            if (reviewer.attrib['id'] == '0'):
-                for aspect in reviewer:
-                    if aspect.attrib.get("category") == "FOOD":
-                        temp_id = "food_0"
-                    if aspect.attrib.get("category") == "PRICE":
-                        temp_id = "price_0"
-                    if aspect.attrib.get("category") == "AMBIENCE":
-                        temp_id = "ambience_0"
-                    if aspect.attrib.get("category") == "SERVICE":
-                        temp_id = "service_0"
-                    if aspect.attrib.get("polarity") == "POSITIVE":
-                        temp_polarity = "positive"
-                    if aspect.attrib.get("polarity") == "NEGATIVE":
-                        temp_polarity = "negative"
-                    row_payload[temp_id] = temp_polarity
-            
-            if (reviewer.attrib['id'] == '1'):
-                for aspect in reviewer:
-                    if aspect.attrib.get("category") == "FOOD":
-                        temp_id = "food_1"
-                    if aspect.attrib.get("category") == "PRICE":
-                        temp_id = "price_1"
-                    if aspect.attrib.get("category") == "AMBIENCE":
-                        temp_id = "ambience_1"
-                    if aspect.attrib.get("category") == "SERVICE":
-                        temp_id = "service_1"
-                    if aspect.attrib.get("polarity") == "POSITIVE":
-                        temp_polarity = "positive"
-                    if aspect.attrib.get("polarity") == "NEGATIVE":
-                        temp_polarity = "negative"
-                    row_payload[temp_id] = temp_polarity
+
+            for aspect in reviewer:
+                temp_id = "_".join(
+                    [
+                        aspect.attrib.get("category").lower(),
+                        reviewer.attrib['id']
+                    ]
+                )
+                temp_polarity = aspect.attrib.get("polarity").lower()    
+                row_payload[temp_id] = temp_polarity
         rows.append(row_payload)
     dataframe = pd.DataFrame(rows, columns = columns)
     return dataframe
@@ -114,7 +90,7 @@ def preprocess_labelled_data(labelled_data: pd.DataFrame) -> pd.DataFrame:
 
     Returns:
         same data structure of labelled_data 
-        but with review the text in the normalized form
+        but with review text in the normalized form
     """
     labelled_data["text"] = _normalize_text_column(labelled_data["text"])
     return labelled_data
