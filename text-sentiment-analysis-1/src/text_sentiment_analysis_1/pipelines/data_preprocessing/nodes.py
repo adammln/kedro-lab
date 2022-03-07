26c63ba2e147ce53c7fc99cc089bd39d835f5b59
Adam Maulana <gitarbetot@gmail.com>
2022-03-07 07:15:41 +0700

    Feat: add custom sw removal to data preprocessing

--- text-sentiment-analysis-1/src/text_sentiment_analysis_1/pipelines/data_preprocessing/nodes.py
+++ text-sentiment-analysis-1/src/text_sentiment_analysis_1/pipelines/data_preprocessing/nodes.py
@@ -77,6 +77,23 @@ def _remove_stopwords_in_text_column(x: pd.Series) -> pd.Series:
     x = x.apply(_remove_stopwords)
     return x
 
+def _remove_custom_stopwords(text: str, stopwords_text: str):
+    stopwords = set(' '.join(stopwords_text.split('\n')))
+    text = ' '.join([word for word in text.split() if word not in stopwords])
+    return text
+
+def _remove_custom_stopwords_in_text_column(
+        data_series: pd.Series,
+        stopwords_text: str
+    ) -> pd.Series:
+
+    def _sw_removal_helper(x: str) -> str:
+            x = _remove_custom_stopwords(x, stopwords_text)
+            return x
+
+    data_series = data_series.apply(_sw_removal_helper)
+    return data_series
+
 def extract_and_convert_labelled_data(xml_content: str) -> pd.DataFrame:
     """ Extract content of XML file of labelled data 
         and convert to pandas Dataframe
@@ -92,7 +109,10 @@ def extract_and_convert_labelled_data(xml_content: str) -> pd.DataFrame:
     dataframe = _convert_labelled_data_tree_to_dataframe(tree)
     return dataframe
 
-def preprocess_labelled_data(labelled_data: pd.DataFrame) -> pd.DataFrame:
+def preprocess_labelled_data(
+        labelled_data: pd.DataFrame,
+        stopwords_custom: str,
+    ) -> pd.DataFrame:
     """Preprocess text data of reviews in labelled_data dataframe 
     
     Args:
@@ -105,8 +125,13 @@ def preprocess_labelled_data(labelled_data: pd.DataFrame) -> pd.DataFrame:
         preprocessing includes:
         - regular expression
         - stopwords removal
-        TODO: add lemmatization & custom stopwords for abbreviated words
+        - custom stopwords removal
+        TODO: add lemmatization
     """
     labelled_data["text"] = _normalize_text_column(labelled_data["text"])
     labelled_data["text"] = _remove_stopwords_in_text_column(labelled_data["text"])
+    labelled_data["text"] = _remove_custom_stopwords_in_text_column(
+        labelled_data["text"], 
+        stopwords_custom
+    )
     return labelled_data
--- text-sentiment-analysis-1/src/text_sentiment_analysis_1/pipelines/data_preprocessing/pipeline.py
+++ text-sentiment-analysis-1/src/text_sentiment_analysis_1/pipelines/data_preprocessing/pipeline.py
@@ -6,7 +6,6 @@ generated using Kedro 0.17.7
 from kedro.pipeline import Pipeline, node, pipeline
 from .nodes import extract_and_convert_labelled_data, preprocess_labelled_data
 
-
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
@@ -18,12 +17,12 @@ def create_pipeline(**kwargs) -> Pipeline:
             ),
             node(
                 func=preprocess_labelled_data,
-                inputs="converted_labelled_data",
+                inputs=["converted_labelled_data", "stopwords_custom"],
                 outputs="preprocessed_labelled_data",
                 name="preprocess_labelled_data_node",
-            )
+            ),
         ],
         namespace="data_preprocessing", 
-        inputs=["labelled_data"],
-        outputs=["converted_labelled_data","preprocessed_labelled_data"],
+        inputs=["labelled_data", "stopwords_custom"],
+        outputs=["converted_labelled_data", "preprocessed_labelled_data"],
     )
