bff7c87f09d2a0bd98d6195f81c8cf7c7fe2ec1e
Adam Maulana <gitarbetot@gmail.com>
2022-03-11 16:56:01 +0700

    Feat: create feature extraction templates

--- text-sentiment-analysis-1/src/text_sentiment_analysis_1/pipelines/data_preprocessing/nodes.py
+++ text-sentiment-analysis-1/src/text_sentiment_analysis_1/pipelines/data_preprocessing/nodes.py
@@ -4,6 +4,7 @@ generated using Kedro 0.17.7
 """
 from lxml import etree
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 import nltk
 import pandas as pd
@@ -224,3 +225,31 @@ def create_testing_data_table(
     )
     testing_data_table = labelled_testing_data.dropna()
     return testing_data_table
+
+def _extract_ngrams(texts: pd.Series) -> pd.Series, np.ndarray:
+    """ Extract N-Gram features
+        with N ranging from 1-3
+    """
+    vectorizer = CountVectorizer(ngram_range=(1,3), max_features=5000)
+    features = vectorizer.fit_transform(np.array(texts)).todense()
+    feature_names = vectorizer.get_feature_names()
+    return texts, feature_names
+
+def _extract_tf_idfs(texts: pd.Series) -> pd.Series:
+    # TODO: create TF-IDF feature extraction
+    """ Extract TF-IDF (Term Frequency — Inverse Document Frequency)
+    """
+    return texts
+
+def _extract_word2vec(texts: pd.Series) -> pd.Series:
+    # TODO: create Word2Vec feature extraction
+    """ Extract word embeddings feature using Word2Vec
+    """
+    return texts
+
+def _extract_bert(texts: pd.Series) -> pd.Series:
+    # TODO: create BERT feature extraction
+    """ Extract word embeddings feature 
+        using Bi-Directional Encoder Representations from Transformer (BERT)
+    """
+    return texts
