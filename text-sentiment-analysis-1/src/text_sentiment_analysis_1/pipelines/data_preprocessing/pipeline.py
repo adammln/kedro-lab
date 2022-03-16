889ec3be3181f1e00a77cdc0d11a008cc4fd4196
Adam Maulana <gitarbetot@gmail.com>
2022-03-16 12:05:33 +0700

    Feat: temporary disable all nodes except data extraction

--- text-sentiment-analysis-1/src/text_sentiment_analysis_1/pipelines/data_preprocessing/pipeline.py
+++ text-sentiment-analysis-1/src/text_sentiment_analysis_1/pipelines/data_preprocessing/pipeline.py
@@ -12,70 +12,75 @@ def create_pipeline(**kwargs) -> Pipeline:
             node(
                 func=extract_and_convert_labelled_data,
                 inputs="labelled_data",
-                outputs="converted_labelled_data",
+                outputs="typed_labelled_data",
                 name="extract_and_convert_labelled_data_node",
-            ),
-            node(
-                func=preprocess_text_column,
-                inputs=["converted_labelled_data", "stopwords_custom"],
-                outputs="preprocessed_labelled_data",
-                name="preprocess_labelled_data_node",
             ),
             node(
                 func=extract_and_convert_testing_data,
                 inputs="testing_data",
-                outputs="converted_testing_data",
+                outputs="typed_testing_data",
                 name="extract_and_convert_testing_data_node",
             ),
-            node(
-                func=preprocess_text_column,
-                inputs=["converted_testing_data", "stopwords_custom"],
-                outputs="preprocessed_testing_data",
-                name="preprocess_testing_data_node",
-            ),
-            node(
-                func=preprocess_gold_standard,
-                inputs="gold_standard",
-                outputs="testing_data_labels",
-                name="preprocess_gold_standard_node",
-            ),
-            node(
-                func=create_testing_data_table,
-                inputs=["preprocessed_testing_data", "testing_data_labels"],
-                outputs="testing_data_table",
-                name="create_testing_data_table_node",
-            ),
-            node(
-                func=extract_train_test_features_from_texts,
-                inputs=[
-                    "preprocessed_labelled_data", 
-                    "testing_data_table"
-                ],
-                outputs=[
-                    "feature_tf_train",
-                    "feature_tf_idf_train",
-                    "feature_tf_test",
-                    "feature_tf_idf_test",
-                    "count_vectorizer",
-                    "tfidf_transformer"
-                ]
-            )
+            # node(
+            #     func=preprocess_text_column,
+            #     inputs=["converted_labelled_data", "stopwords_custom"],
+            #     outputs="preprocessed_labelled_data",
+            #     name="preprocess_labelled_data_node",
+            # ),
+            # node(
+            #     func=preprocess_text_column,
+            #     inputs=["converted_testing_data", "stopwords_custom"],
+            #     outputs="preprocessed_testing_data",
+            #     name="preprocess_testing_data_node",
+            # ),
+            # node(
+            #     func=preprocess_gold_standard,
+            #     inputs="gold_standard",
+            #     outputs="testing_data_labels",
+            #     name="preprocess_gold_standard_node",
+            # ),
+            # node(
+            #     func=create_testing_data_table,
+            #     inputs=["preprocessed_testing_data", "testing_data_labels"],
+            #     outputs="testing_data_table",
+            #     name="create_testing_data_table_node",
+            # ),
+            # node(
+            #     func=extract_train_test_features_from_texts,
+            #     inputs=[
+            #         "preprocessed_labelled_data", 
+            #         "testing_data_table"
+            #     ],
+            #     outputs=[
+            #         "feature_tf_train",
+            #         "feature_tf_idf_train",
+            #         "feature_tf_test",
+            #         "feature_tf_idf_test",
+            #         "count_vectorizer",
+            #         "tfidf_transformer"
+            #     ]
+            # )
         ],
         namespace="data_preprocessing", 
         inputs=[
+            # "unlabelled_data",
             "labelled_data",
             "testing_data",
-            "stopwords_custom",
-            "gold_standard"
+            # "stopwords_custom",
+            # "gold_standard"
         ],
         outputs=[
-            "preprocessed_labelled_data",
-            "testing_data_table",
-            "feature_tf_train",
-            "feature_tf_idf_train",
-            "feature_tf_test",
-            "feature_tf_idf_test",
-            "count_vectorizer",
-            "tfidf_transformer"
+            # "preprocessed_labelled_data",
+            # "typed_unlabelled_data",
+            "typed_labelled_data",
+            "typed_testing_data",
+            # "testing_data_table",
+            # "converted_testing_data",
+            # "feature_tf_train",
+            # "feature_tf_idf_train",
+            # "feature_tf_test",
+            # "feature_tf_idf_test",
+            # "count_vectorizer",
+            # "tfidf_transformer"
         ],
     )
